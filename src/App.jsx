6f50d9f35dca1da66a97cbea154542b9bbfe51a2
natalia galindo <nataliadgalindo@outlook.com>
2023-10-23 22:20:34 +0100

    update landmarks

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,10 @@ function App() {
   return (
     <>
       <Header />
-      <About />
-      <Creations />
+      <main>
+        <About />
+        <Creations />
+      </main>
       <Footer />
     </>
   )
